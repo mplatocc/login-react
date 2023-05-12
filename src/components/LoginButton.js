27ee89e90acd5a67d001f499e8753df08fa6b5a1
jcgomez96 <julcgomez96@gmail.com>
2023-05-12 11:13:04 -0600

    add changes

--- src/components/LoginButton.js
+++ src/components/LoginButton.js
@@ -3,8 +3,13 @@ import {useAuth0} from '@auth0/auth0-react';
 const LoginButton = () => {
     const { loginWithRedirect } = useAuth0();
     return(
-        <button 
-        onClick={()=> loginWithRedirect()}>Login</button>
+       <div>
+       <h1>Universidad Mariano Gálvez de Guatemala</h1>
+       <h2>Maestría en Seguridad Informática</h2>
+       <button 
+       onClick={()=> loginWithRedirect()}>Login</button>
+       </div>
+       
     )
 }
 
